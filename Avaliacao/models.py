a912a44ba14a1e5279d268d7beea72f307543195
Kaique-py <66974354+Kaique-py@users.noreply.github.com>
2023-05-24 23:45:41 -0300

    Registrando o app Avaliacao

--- Avaliacao/models.py
+++ Avaliacao/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 #from users.models import User
 #from core.models import Anuncio
 
@@ -6,14 +7,15 @@ from django.db import models
 
 class Avaliacao(models.Model):
     #Usuario que está sendo avaliado (vendedor)
-    #usuario_avaliado = models.ForeignKey(User, on_delete=models.CASCADE)
+    usuario_avaliado = models.ForeignKey(User, on_delete=models.CASCADE)
 
     #Usuario que está avaliando (comprador)
-    #usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
     nota = models.IntegerField(default=0) #Como você avalia o vendedor.
     notaNegociacao = models.IntegerField(default=5) #Como a negociação fluiu.
     notaClickAutos = models.IntegerField(default=5) #Como você avalia a ClickAutos.
+    comentario = models.TextField(null=True, blank=True)
     aprovada = models.BooleanField(default=False)
 
     #anuncio = models.ForeignKey(Anuncio, on_delete=models.CASCADE)
@@ -22,8 +24,8 @@ class Avaliacao(models.Model):
     expira_em = models.DateTimeField(blank=True)
 
     class Meta:
-        ordering = ('usuario_avaliado',) #Aqui ordenamos as categorias em ordem alfabética.
-        verbose_name_plural = 'Avaliações'
+        ordering = ('usuario_avaliado',) #Aqui ordenamos pelo nome do vendedor, em ordem alfabética.
+        verbose_name_plural = 'Avaliações' #Só pra deixar o plural correto.
 
     def __str__(self):
         return self.usuario_avaliado
